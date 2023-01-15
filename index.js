7f62cbfd0233c86241d7b7d9665fc4178bff3d5c
ahmed yehia morsy <110130294+Ahmedyehya1@users.noreply.github.com>
2023-01-15 17:59:51 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+ScrollReveal().reveal('.move-text', {
+  duration: 3000,
+  origin: 'top',
+  delay: 100,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+
+ScrollReveal().reveal('.move-img', {
+  duration: 3000,
+  origin: 'top',
+  delay: 80,
+  rotate: {
+    x: 20,
+    z: 100
+  }
+});
+
+ScrollReveal().reveal('.move-nav', {
+  duration: 3000,
+  origin: 'top',
+  delay: 80,
+  rotate: {
+    x: 20,
+    z: 20
+  }
+});
+
+ScrollReveal({
+  distance: '60px'
+});
+ScrollReveal().reveal('.move-left', {
+  origin: 'left'
+});
+ScrollReveal().reveal('.move-left', {
+  duration: 3000,
+  delay: 80
+});
+
+ScrollReveal({
+  distance: '60px'
+});
+ScrollReveal().reveal('.move-right', {
+  origin: 'right'
+});
+ScrollReveal().reveal('.move-right', {
+  duration: 3000,
+  delay: 80
+});
+
+ScrollReveal({
+  distance: '60px'
+});
+ScrollReveal().reveal('.move-bottom', {
+  origin: 'bottom'
+});
+ScrollReveal().reveal('.move-bottom', {
+  duration: 3000,
+  delay: 80
+});
+
+ScrollReveal({
+  distance: '60px'
+});
+ScrollReveal().reveal('.move-up', {
+  origin: 'top'
+});
+ScrollReveal().reveal('.move-up', {
+  duration: 3000,
+  delay: 80
+});
